a7a750c0af81d6f478ebdbfd8c3e4b48a925b6ae
Joice-crypto <joyciinha_cristina@hotmail.com>
2021-05-16 00:01:28 -0300

    Iniciando meu curso de C#

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// Da pra comentar assim
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // da pra comentar assim 
+           
+            Console.WriteLine("Vamos falar sobre comentarios");
+            Console.WriteLine("O C# Tem o XML  comments");
+        }
+    }
+}
---
+++ Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar()
+        {
+            double raio = 4.5; // variavel para numeros quebrados
+            const double PI = 3.14; // define uma constante ou sejna nao pode ser alterada
+
+            raio = 5.5;
+            double area = PI * raio * raio;
+            Console.WriteLine("A area é " + area);
+
+            /*Variaveis numericas
+             * byte (1 unico byte)
+             * short (2 bytes)
+             * int (4 bytes)
+             * long (8 byte)
+             */
+
+
+        }
+    }
+}
